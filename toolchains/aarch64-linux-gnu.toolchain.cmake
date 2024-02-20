f5e7bf546ec75f6b1a2ec794c383d87475e36a8c
Helco <hermann.noll@hotmail.com>
2024-02-20 19:45:13 +0100

    Install and use aarch64 objcopy

--- toolchains/aarch64-linux-gnu.toolchain.cmake
+++ toolchains/aarch64-linux-gnu.toolchain.cmake
@@ -3,6 +3,7 @@ set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
 set(CMAKE_C_COMPILER "aarch64-linux-gnu-gcc")
 set(CMAKE_CXX_COMPILER "aarch64-linux-gnu-g++")
+set(CMAKE_OBJCOPY "aarch64-linux-gnu-objcopy")
 
 set(CMAKE_C_FLAGS "-march=armv8-a")
 set(CMAKE_CXX_FLAGS "-march=armv8-a")
