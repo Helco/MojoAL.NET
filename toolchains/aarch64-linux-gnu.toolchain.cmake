62ec91bf4abf71f4960119bfab919f2dd44ecfa0
Helco <hermann.noll@hotmail.com>
2024-02-20 19:58:09 +0100

    Try setting CROSS_COMPILE env variable for libcap

--- toolchains/aarch64-linux-gnu.toolchain.cmake
+++ toolchains/aarch64-linux-gnu.toolchain.cmake
@@ -5,6 +5,8 @@ set(CMAKE_C_COMPILER "aarch64-linux-gnu-gcc")
 set(CMAKE_CXX_COMPILER "aarch64-linux-gnu-g++")
 set(CMAKE_OBJCOPY "aarch64-linux-gnu-objcopy")
 
+set(ENV{CROSS_COMPILE} "aarch64-linux-gnu-")
+
 set(CMAKE_C_FLAGS "-march=armv8-a")
 set(CMAKE_CXX_FLAGS "-march=armv8-a")
 
